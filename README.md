76d9da6a8487688b855b19b77e152e322c404e28
陶柯宇 <tky753@qq.com>
2021-04-19 13:41:06 +0800

    feat: update README.md

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Tky.Dapr.ReverseProxy
 a simple sample for using Dapr and yarp together.
+
+# steps
+- $ cd test
+- $ tye run
--- test/GatewayDemo/Startup.cs
+++ test/GatewayDemo/Startup.cs
@@ -12,16 +12,11 @@ namespace GatewayDemo
     {
         private readonly IConfiguration _configuration;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Startup" /> class.
-        /// </summary>
         public Startup(IConfiguration configuration)
         {
             _configuration = configuration;
         }
         
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddReverseProxy()
@@ -29,7 +24,6 @@ namespace GatewayDemo
                 .AddTransforms<DaprTransformProvider>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
